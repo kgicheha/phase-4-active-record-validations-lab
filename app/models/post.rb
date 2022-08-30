cfdd2fda2cad087658470fa30e1ccc4cbbafe204
Kevin Gicheha <gichehakevin@gmail.com>
2022-08-30 09:19:42 -0700

    commit tree 44e41b4535c2719427679daf983c7fbc3ddae9c9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates_length_of :content, :minimum => 250
+    validates_length_of :summary, :maximum => 250
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :clickbait?
+
+
+    CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+    ]
+
+    def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pattern| pattern.match title}
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
